9974458f6ffaff344d29e7a3af5a55a832d55e23
rvmzik <ramazan.aliyev.2001@bk.ru>
2023-08-31 21:33:27 +0600

    Initial Commit

---
+++ dice app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice app
+//
+//  Created by Рамазан Алиев on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
